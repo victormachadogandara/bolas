cafafa6ce62da41cb63a94875e1e3814db2b1a0f
SEBASTIAN NIEBLAS ZAZUETA <sebas_nit_040899@hotmail.com>
2019-02-25 17:32:34 -0700

    hola

---
+++ Assets/Script.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    Rigidbody2D rb2D;
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        rb2D.AddForce(Vector2.left * 100, ForceMode2D.Force);
+    }
+}
